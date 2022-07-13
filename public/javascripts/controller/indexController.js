bca0c5bb9ec57aef6660c08858b42da053153e8e
brkgcl <burakguclu05@hotmail.com>
2022-07-14 00:05:35 +0300

    Game Area

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,6 +1,7 @@
 app.controller('indexController', ['$scope', 'indexFactory',($scope, indexFactory) => {
 
-    $scope.messages = [ ]
+    $scope.messages = [ ];
+    $scope.players = { };
 
     $scope.init = () => {
         const username = prompt("Please enter username");
@@ -20,15 +21,36 @@ app.controller('indexController', ['$scope', 'indexFactory',($scope, indexFactor
             .then((socket) => {
                 socket.emit('newUser', { username });
 
+                socket.on('initPlayers', (players) => {
+                    $scope.players = players;
+                    $scope.$apply();
+                    console.log(players);
+                })
+
                 socket.on('newUser', (data) => {
                     const messageData = {
-                        type: 0,
+                        type: {
+                            code: 0,    //server or user message
+                            message: 1 // login or disconnect message
+                        },
                         username: data.username
                     };
 
                     $scope.messages.push(messageData);
                     $scope.$apply();
                 });
+
+                socket.on('disUser', (data) => {
+                    const messageData = {
+                        type: {
+                            code: 0,    //server or user message
+                            message: 0 // login or disconnect message
+                        },
+                        username: data.username
+                    };
+                    $scope.messages.push(messageData);
+                    $scope.$apply();
+                })
             }).catch((err) => {
                 console.log(err);
             })
--- src/socketApi.js
+++ src/socketApi.js
@@ -5,7 +5,7 @@ const socketApi = {};
 
 socketApi.io = io;
 
-const users = [ ];
+const users = { };
 
 io.on('connection', (socket) => {
     console.log("a user connected");
@@ -20,9 +20,18 @@ io.on('connection', (socket) => {
         }
 
         const userData = Object.assign(data, defaultData);
-        users.push(userData);
+        users[socket.id] = userData;
+        console.log(users)
         
-        socket.broadcast.emit('newUser', userData)
+        socket.broadcast.emit('newUser', users[socket.id]);
+        socket.emit('initPlayers', users);
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('disUser', users[socket.id]);
+        delete users[socket.id];
+
+        console.log(users);
     });
 });
 
